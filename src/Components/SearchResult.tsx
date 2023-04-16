849a978856b0ab1496cea5684dc552dc82878b1c
Sagar Mainali <sagarmainali78@gmail.com>
2023-04-16 19:28:41 +0545

    dummy data insertion and completed design

--- src/Components/SearchResult.tsx
+++ src/Components/SearchResult.tsx
@@ -10,31 +10,53 @@ function SearchResult() {
 
       <div className="data-container">
         <div className="number-data">
-          <div className="repo">
-
+          <div className="nd repos">
+            <span>22</span>
+            <p>Repos</p>
           </div>
-          <div className="followers">
-
+          <div className="nd followers">
+            <span>18</span>
+            <p>Followers</p>
           </div>
-          <div className="following">
-
+          <div className="nd following">
+            <span>8</span>
+            <p>Following</p>
           </div>
         </div>
 
         <div className="basic-info">
-
+          <h2>Sagar Mainali</h2>
+          <div className="link-jd">
+            <a href="">@Sagar Mainali</a>
+            <span>Joined - 14 Apr, 2023</span>
+          </div>
         </div>
 
         <div className="bio">
-
+          Computer Enthusiast
         </div>
+
 
         <div className="additional-info">
-
+          <hr className="hr-top"/>
+          <div className="ai-ul">
+            <ul className="first-ul">
+              <li className="location"><i className="fa-solid fa-location-dot"></i>Kathmandu</li>
+              <li className="office"><i className="fa-solid fa-building"></i>Not available</li>
+              <li className="email"><i className="fa-solid fa-envelope"></i>Not available</li>
+            </ul>
+            <ul className="second-ul">
+              <li className="blog"><i className="fa-solid fa-link"></i>Not available</li>
+              <li className="twitter"><i className="fa-brands fa-twitter"></i>Not available</li>
+              <li className="hireable"><i className="fa-solid fa-check"></i>Hireable</li>
+            </ul>
+          </div>
+          <hr className="hr-bottom"/>
         </div>
 
-        <div className="last-activity">
 
+        <div className="last-activity">
+          27 June, 2023
         </div>
       </div>
     </div>
@@ -43,10 +65,4 @@ function SearchResult() {
 
 export default SearchResult
 
-{/* <i className="fa-solid fa-moon"></i>
-< i className="fa-solid fa-location-dot" ></ >
-<i className="fa-solid fa-building"></i>
-<i className="fa-solid fa-check"></i>
-<i className="fa-solid fa-envelope"></i>
-<i className="fa-solid fa-link"></i>
-<i className="fa-brands fa-twitter"></i> */}
+{/* <i className="fa-solid fa-moon"></i> */ }
