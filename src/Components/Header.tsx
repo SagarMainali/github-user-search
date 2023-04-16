e42af61a4c66d57573aa34f3a24d81de5b280c6c
Sagar Mainali <sagarmainali78@gmail.com>
2023-04-16 17:01:20 +0545

    required icons added

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,12 +1,22 @@
 function Header() {
   return (
     <div className="header">
+      <i className="fa-brands fa-github"></i>
       <div className="input-container">
-        
         <input type="text" placeholder="Search Users" />
+        <i className="fa-solid fa-magnifying-glass"></i>
       </div>
+      <i className="fa-solid fa-sun"></i>
     </div>
   )
 }
 
 export default Header
+
+{/* <i className="fa-solid fa-moon"></i>
+< i className="fa-solid fa-location-dot" ></ >
+<i className="fa-solid fa-building"></i>
+<i className="fa-solid fa-check"></i>
+<i className="fa-solid fa-envelope"></i>
+<i className="fa-solid fa-link"></i>
+<i className="fa-brands fa-twitter"></i> */}
