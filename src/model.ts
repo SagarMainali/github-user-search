82cb3ac329a375f2d9550f0fcec80a67209be3e2
Sagar Mainali <sagarmainali78@gmail.com>
2023-04-17 12:04:53 +0545

    model update

---
+++ src/model.ts
@@ -0,0 +1,19 @@
+type UserData = {
+  avatar_url: string,
+  public_repos: number,
+  followers: number,
+  following: number,
+  name: string,
+  login: string,
+  html_url: string,
+  created_at: string,
+  bio: string | null,
+  location: string | null,
+  company: string | null,
+  email: string | null,
+  blog: string | null,
+  twitter_username: string | null,
+  updated_at: string,
+}
+
+export default UserData
